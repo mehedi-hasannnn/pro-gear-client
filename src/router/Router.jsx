4463b9ccdeb3d62d704d634e5815575f0e8d705b
mehedi-hasannnn <mhasan2999@gmail.com>
2025-04-28 14:14:29 +0600

    navbar and basic folder structure setup done

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,19 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import Root from "../layout/Root";
+import Home from "../layout/Home";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>,
+        }
+      ],
     },
   ]);
 
