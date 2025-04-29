5496c83d572d77cd5b2918129c00988456cbe7e9
mehedi-hasannnn <mhasan2999@gmail.com>
2025-04-29 14:55:41 +0600

    equipment list and table and error page added

---
+++ src/page/EquipmentTable.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+
+const EquipmentTable = ({equipment,idx}) => {
+  const navigate = useNavigate()
+    const {_id,name,category,price} = equipment;
+    
+    const handleDetails = (id) =>{
+      navigate(`/equipment/${id}/details`)
+    }
+  return (
+    <>
+      <tr>
+        <td>{idx + 1}</td>
+        <td>{name}</td>
+        <td>{category}</td>
+        <td>${price}</td>
+        <td>
+            <button onClick={()=>handleDetails(_id)} className="bg-[#273248] hover:text-[#273248] hover:bg-red-400  px-4 py-2 rounded text-white">Details</button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+EquipmentTable.propTypes = {
+    equipment: PropTypes.object.isRequired,
+    idx: PropTypes.string.isRequired
+};
+
+export default EquipmentTable;
